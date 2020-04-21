6b7f1fd733ecbf486acef41b65fee9084cf2cfa2
Хорошев Евгений <khoroshev.e.i@gmail.com>
2020-04-22 00:08:18 +0300

    Adding heroku hosting url

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,23 +1,39 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace Gateway
 {
     public class Program
     {
-        private const string _defaultHostUrl = "http://ads-app.local:5001";
+        private const string _defaultHostUrl = "https://betonomeshalka.herokuapp.com/";
 
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            try
+            {
+                var webHost = new WebHostBuilder()
+                    .UseKestrel()
+                    .UseStartup<Startup>()
+                    .Start(_defaultHostUrl);
+
+                webHost.WaitForShutdown();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseUrls(_defaultHostUrl);
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls(_defaultHostUrl)
+                        .PreferHostingUrls(true)
+                        .UseStartup<Startup>();
                 });
     }
 }
