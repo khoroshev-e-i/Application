a93a22df09c2c8de325707a330a7cdb304fe3d04
Хорошев Евгений <khoroshev.e.i@gmail.com>
2020-04-21 23:51:47 +0300

    adding heroku url

--- Angular/Program.cs
+++ Angular/Program.cs
@@ -1,16 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Angular
 {
     public class Program
     {
+        private static readonly string _herokuApiUrl = "https://betonomeshalka.herokuapp.com/";
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -21,6 +17,8 @@ namespace Angular
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls(_herokuApiUrl);
+                    webBuilder.PreferHostingUrls(true);
                 });
     }
 }
